3d9b68f577952fed5766a71f026f215784aebb3c
FelipeParreiras <felipe.parreiras04@gmail.com>
2024-05-13 13:10:27 -0300

    estudando multiplos catch famila de catch e catch generico

---
+++ EstudandoJava_LoianeTraining/src/aula26/CatchGenerico.java
@@ -0,0 +1,23 @@
+package aula26;
+
+public class CatchGenerico {
+
+	public static void main(String[] args) {
+
+		int[] numeros = {4,8,16,32,64,128};
+		int[] demon = {2,0,4,8,0};
+		
+		for (int i = 0; i < numeros.length; i++) {
+			try {
+				System.out.println(numeros[i] + " / "+ demon[i] + " = " + (numeros[i]/demon[i]));
+			}catch (ArithmeticException e1) {
+				System.out.println("Erro ao dividir por 0");
+			}//Exception Genérica
+			catch(Throwable e2){
+				System.out.println("Ocorreu um erro genérico");
+			}
+			
+		}
+	}
+
+}
